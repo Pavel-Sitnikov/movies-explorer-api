69924ef4e00391d970bb4e444d26b840fb8705d1
Pavel Sitnikov <sitnikovpa@gmail.com>
2022-10-16 20:40:49 +0500

    feat: изменить адерс

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { PORT, MONGO_ADRESS } = require('./utils/constants');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://api.diplom-pavel.nomoredomains.icu',
   credentials: true,
 }));
 
