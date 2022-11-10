bcea4459b30f7fbd8c9d056321518246efb98ee1
Pavel Sitnikov <sitnikovpa@gmail.com>
2022-11-10 22:23:26 +0500

    fix: изменить cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { PORT = 3000, MONGO_ADRESS = 'mongodb://localhost:27017/moviesdb' } = pro
 const app = express();
 
 app.use(cors({
-  origin: 'https://api.diplom-pavel.nomoredomains.icu',
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
 
